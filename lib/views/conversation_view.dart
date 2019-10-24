b2ff99ccf4f7eb51c009fcdbd16ac6a6d0d51523
tmkch <tomekch6@gmail.com>
2019-10-24 20:51:11 +0200

    Add basic input text field

--- lib/views/conversation_view.dart
+++ lib/views/conversation_view.dart
@@ -17,21 +17,59 @@ class ConversationView extends StatelessWidget {
           onPressed: () => Navigator.of(context).pop(),
         ),
       ),
-      body: ScopedModelDescendant<AppModel>(
-        builder: (context, child, model) {
-          final convo = model.convoWith(partner);
-          return ListView.builder(
-            itemBuilder: (context, index) =>
-                /* ListTile(
-              title: Text(convo.messages[index].isOwned ? 'Ja' : partner),
-              subtitle: Text(convo.messages[index].content),
-            ),*/
-                ChatBubble(
-              message: convo.messages[index],
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            child: ScopedModelDescendant<AppModel>(
+              builder: (context, child, model) {
+                final convo = model.convoWith(partner);
+                return ListView.builder(
+                  itemBuilder: (context, index) => ChatBubble(
+                    message: convo.messages[index],
+                  ),
+                  itemCount: convo.messages.length,
+                );
+              },
             ),
-            itemCount: convo.messages.length,
-          );
-        },
+          ),
+          Row(
+            children: <Widget>[
+              Flexible(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8),
+                  child: TextField(
+                    style: TextStyle(fontSize: 16),
+                    decoration: InputDecoration(
+                      hintText: 'Aa',
+                      contentPadding:
+                          EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                      filled: true,
+                      fillColor: Theme.of(context).cardColor,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Theme.of(context).textTheme.display1.color,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Theme.of(context).accentColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              IconButton(
+                icon: Icon(Icons.send),
+                onPressed: () => null,
+              )
+            ],
+          )
+        ],
       ),
     );
   }
